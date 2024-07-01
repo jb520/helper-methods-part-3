1a82d1dfd4ef9f5ea4df7159940b498c5c6ead97
jb520 <jnavarro1995@gmail.com>
2024-06-30 19:37:56 -0500

    add directors class

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,6 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+
   def new
     @movie = Movie.new
   end
@@ -14,7 +16,6 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = Movie.find(params.fetch(:id))
   end
 
   def create
@@ -32,11 +33,9 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    @movie = Movie.find(params.fetch(:id))
   end
 
   def update
-    @movie = Movie.find(params.fetch(:id))
 
     #movie_params = params.require(:movie).permit(:title, :description, :image_url)
     
@@ -48,7 +47,6 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    @movie = Movie.find(params.fetch(:id))
 
     @movie.destroy
 
@@ -58,6 +56,10 @@ class MoviesController < ApplicationController
   private
   def movie_params
     params.require(:movie).permit(:title, :description, :image_url, :released_on)
+  end
+
+  def set_movie
+    @movie = Movie.find(params.fetch(:id))
   end
 
 end
